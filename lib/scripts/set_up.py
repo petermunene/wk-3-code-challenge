d92be06822725a6ef98d959f8f2f90f96a148886
petermunene <peter.munene@student.moringaschool.com>
2025-05-27 22:17:41 +0300

    path correction

--- lib/scripts/set_up.py
+++ lib/scripts/set_up.py
@@ -3,7 +3,7 @@ def setup_database():
     conn = sqlite3.connect("project.db")
     cursor = conn.cursor()
 
-    with open("lib/db/schema.sql", "r") as f:
+    with open("lib/db-management/schema.sql", "r") as f:
         schema = f.read()
         cursor.executescript(schema)
 
